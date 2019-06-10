e232e16d3cb8812b7d0b5c4616ad9c74831df02e
Derek-J <lutics@users.noreply.github.com>
2019-06-10 14:12:59 +0900

    Lesson 2 - 9. Quiz: Add Interactivity to Your Unit Converter

--- category_widget/lib/category.dart
+++ category_widget/lib/category.dart
@@ -42,6 +42,7 @@ class Category extends StatelessWidget {
             name: name,
             units: units,
           ),
+          resizeToAvoidBottomPadding: false,
         );
       },
     ));
@@ -55,8 +56,8 @@ class Category extends StatelessWidget {
         height: _rowHeight,
         child: InkWell(
           borderRadius: _borderRadius,
-          highlightColor: color,
-          splashColor: color,
+          highlightColor: color['highlight'],
+          splashColor: color['splash'],
           onTap: () => _navigateToConverter(context),
           child: Padding(
             padding: EdgeInsets.all(8.0),
